06e3bca94d8be28d6d023dfc61c41668423a260a
raymond poon <raymondpoon95@hotmail.co.uk>
2026-08-29 13:57:15 +0100

    overview + demo table

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import DataTable from "@/components/ui/DataTable";
+import Image from "next/image";
 
 const Page = () => {
-  return <div className="text-3xl text-indigo-500"></div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="bitcoin"
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$89,000</h1>
+            </div>
+          </div>
+        </div>
+        <p>Trending Coins</p>
+        <DataTable />
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
